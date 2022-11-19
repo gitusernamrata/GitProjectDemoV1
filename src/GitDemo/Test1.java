fd9a78fc65eda7025210adb4ad367fbecb1c7276
Namrata <namratashrimali08@gmail.com>
2022-11-19 12:22:45 +0530

    First Commit

---
+++ src/GitDemo/Test1.java
@@ -0,0 +1,18 @@
+package GitDemo;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hungry = "yes";
+		if(hungry == "yes")
+			System.out.println("Eat something....");
+		else
+		{
+			System.out.println("Do Work...");
+		}
+		
+
+	}
+
+}
